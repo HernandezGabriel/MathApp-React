9895f4b6efc3738211d287cbf70eabe062ebc7e7
Daniel <leedanielt@yahoo.com>
2022-08-18 14:45:02 -0500

    react build start

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+
+class Footer extends Component {
+    render() {
+        return(
+            <footer>
+                Copyright &copy; 2020 LearnIT. All Rights Reserved.
+            </footer>
+        )
+    }
+}
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+
+class Header extends Component {
+    render() {
+        return(
+    <div>
+    <header>
+    <h1>Math Practice</h1>
+    <p class="show-hide">Please select an answer below the problem by clicking on the box</p>
+  </header>
+  </div>
+        )
+   }
+}
+export default Header;
